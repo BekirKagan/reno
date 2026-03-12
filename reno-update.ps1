99d1d1c76837a9937fbe9c6b51ad3e4b38777f87
BekirKagan <kagankaraahmetoglu@hotmail.com>
2026-03-12 18:25:19 +0300

    Actually fixed Winget output not showing in the terminal window.

--- reno-update.ps1
+++ reno-update.ps1
@@ -4,8 +4,8 @@ $projectName = 'Reno'
 $logoPath = Join-Path $PSScriptRoot "$($projectName.ToLower())-logo.png"
 
 try {
-    $result = winget upgrade --all --include-unknown | Tee-Object -Variable output
-    $failed = $result | Select-String -Pattern 'failed' -Quiet
+    winget upgrade --all --include-unknown | Tee-Object -Variable output
+    $failed = $output | Select-String -Pattern 'failed' -Quiet
     if ($failed) {
         New-BurntToastNotification `
             -Text $projectName, 'Some updates failed. Check the winget logs for details.' `
